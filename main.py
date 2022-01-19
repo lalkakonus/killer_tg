8b23ae73ab0e845f753e73efd87cd406089c375c
sergey <lalkakonus@gmail.com>
2022-01-20 01:15:11 +0300

    He talks!

--- main.py
+++ main.py
@@ -16,21 +16,21 @@ AUTH, FINISH_AUTH, COMPLETE, ERROR = range(4)
 # 		["79166835422", "Степан Зимов", None, None],
 # ]
 # db = pd.DataFrame(data, columns=["phone", "name", "tg_account", "tg_user_id"]).set_index("phone")
-db = pd.read_csv("data.csv", sep="\t", index_col=0, header=0, dtype=str)
+db = pd.read_csv("data/data.csv", sep="\t", index_col=0, header=0, dtype=str)
+db.set_index(db.index.astype(str), inplace=True)
 
 
-def start(update: Update, context: CallbackContext):
+def start(update: Update, context: CallbackContext) -> int:
 	if str(update.message.from_user.id) not in db["tg_user_id"].values:
 		request_phone_number(update, context)
 		return AUTH
 	else:
 		context.bot.send_message(chat_id=update.effective_chat.id,
-		                         text="Ты уже зарегестрирован в игру, если хочешь отменить"
-		                              " участие - введи команду /restart")
+		                         text="Ты уже зарегестрирован в игру.")
 		return COMPLETE
 
 
-def request_phone_number(update: Update, context: CallbackContext):
+def request_phone_number(update: Update, context: CallbackContext) -> int:
 	update.message.reply_text(
 		"Привет! Отправь, пожалуйста, свои контактные данные для идентефикации.",
 		reply_markup=ReplyKeyboardMarkup(
@@ -42,7 +42,7 @@ def request_phone_number(update: Update, context: CallbackContext):
 	return AUTH
 
 
-def auth(update: Update, context: CallbackContext):
+def auth(update: Update, context: CallbackContext) -> int:
 	user_id = update.message.from_user.id
 	contact_user_id = update.message.contact.user_id
 	contact_phone_number = update.message.contact.phone_number
@@ -51,12 +51,14 @@ def auth(update: Update, context: CallbackContext):
 		context.bot.send_message(chat_id=update.effective_chat.id,
 		                         text="Ошибка: user_id контакта не совпадает с user_id отправителя.",
 		                         reply_markup=ReplyKeyboardRemove())
+		error_state(update, context)
 		return ERROR
 
 	if contact_phone_number not in db.index:
 		context.bot.send_message(chat_id=update.effective_chat.id,
 		                         text="Ошибка: номер телефона отсутсвует в базе данных.",
 		                         reply_markup=ReplyKeyboardRemove())
+		error_state(update, context)
 		return ERROR
 
 	context.user_data["phone_number"] = contact_phone_number
@@ -64,7 +66,7 @@ def auth(update: Update, context: CallbackContext):
 	return FINISH_AUTH
 
 
-def request_name_approve(update: Update, context: CallbackContext):
+def request_name_approve(update: Update, context: CallbackContext) -> int:
 	name = db.loc[context.user_data["phone_number"]]["name"]
 	update.message.reply_text(
 		"Тебя зовут {name}, верно?".format(name=name),
@@ -77,13 +79,14 @@ def request_name_approve(update: Update, context: CallbackContext):
 	return FINISH_AUTH
 
 
-def finish_auth(update: Update, context: CallbackContext):
+def finish_auth(update: Update, context: CallbackContext) -> int:
 	if update.message.text == "Да":
 		phone_number = context.user_data["phone_number"]
 		db.loc[phone_number]["tg_account"] = update.message.from_user.username
 		db.loc[phone_number]["tg_user_id"] = update.message.from_user.id
 		db.to_csv("data.csv", sep="\t")
 		context.bot.send_message(chat_id=update.effective_chat.id, text="Отлично, ты записан в игру.")
+		return COMPLETE
 	elif update.message.text == "Нет":
 		context.bot.send_message(chat_id=update.effective_chat.id,
 		                         text="Отчёт об ошибке будет отправлени организатору игры."
@@ -94,68 +97,55 @@ def finish_auth(update: Update, context: CallbackContext):
 		raise ValueError("Unexpected argument")
 
 
-def error_state(update: Update, context: CallbackContext) -> int:
-	update.message.reply_text('Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove())
-	return ConversationHandler.END
-
-
-def print_rules(update: Update, context: CallbackContext):
+def print_rules(update: Update, context: CallbackContext) -> int:
 	context.bot.send_message(chat_id=update.effective_chat.id,
 	                         parse_mode=ParseMode.MARKDOWN_V2,
 	                         text="__Правила проведения игры__")
+	return COMPLETE
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-	"""Send a message when the command /help is issued."""
+def help_command(update: Update, context: CallbackContext) -> int:
 	context.bot.send_message(chat_id=update.effective_chat.id, text="Список доступных комманд:\n\help\n\start.")
+	return COMPLETE
 
 
-def cancel(update: Update, context: CallbackContext) -> int:
-	update.message.reply_text('Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove())
-	return ConversationHandler.END
-
-
-def handle_error(update: Update, context: CallbackContext) -> int:
-	update.message.reply_text('Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove())
-	return ConversationHandler.END
+def error_state(update: Update, context: CallbackContext) -> int:
+	context.bot.send_message(chat_id=update.effective_chat.id,
+	                         text="Отчёт об ошибке будет отправлени организатору игры."
+	                              "Проблему исправят в ближайшее время.")
+	return ERROR
 
 
 def restart(update: Update, context: CallbackContext):
-	name = db.loc[context.user_data["phone_number"]]["name"]
-	update.message.reply_text(
-		"Тебя зовут {name}, верно?".format(name=name),
-		reply_markup=ReplyKeyboardMarkup(
-			[["Да", "Нет"], ],
-			one_time_keyboard=True,
-			resize_keyboard=True
-		),
-	)
-	return AUTH
+	db.loc[db["tg_user_id"] == str(update.message.from_user.id), "tg_account"] = None
+	db.loc[db["tg_user_id"] == str(update.message.from_user.id), "tg_user_id"] = None
+	db.to_csv("data.csv", sep="\t")
+	return start(update, context)
 
 
 def main():
-	TOKEN = "5006115022:AAF-idB7Cft_vrJ8QPFxB9X0CM72JtC8Kx8"
+	TOKEN = ""
 	updater = Updater(token=TOKEN)
 	dispatcher = updater.dispatcher
 
 	conv_handler = ConversationHandler(
 		entry_points=[CommandHandler('start', start)],
 		states={
-			AUTH:         [
-				MessageHandler(Filters.contact, auth), ],
-				# MessageHandler(~Filters.contact & ~Filters.regex('/cancel'), request_phone_number), ],
+			AUTH: [
+				MessageHandler(Filters.contact, auth),
+				MessageHandler(~Filters.contact, request_phone_number), ],
 			FINISH_AUTH: [
 				MessageHandler(Filters.regex('[Да|Нет]'), finish_auth),
-				MessageHandler(~Filters.regex('[Да|Нет]') & ~Filters.regex('/cancel'), request_name_approve)	],
-			COMPLETE:    [
+				MessageHandler(~Filters.regex('[Да|Нет]'), request_name_approve), ],
+			COMPLETE: [
 				CommandHandler('rules', print_rules),
 				CommandHandler('help', help_command),
-				CommandHandler('restart', handle_error), ],
-			ERROR:       [
-				MessageHandler(Filters.text, error_state),
-				CommandHandler('restart', restart), ]
+				CommandHandler('restart', restart), ],
+			ERROR: [
+				CommandHandler('restart', restart),
+				MessageHandler(Filters.text, error_state), ]
 		},
-		fallbacks=[CommandHandler('cancel', cancel)],
+		fallbacks=[CommandHandler('restart', restart)],
 	)
 
 	dispatcher.add_handler(conv_handler)
